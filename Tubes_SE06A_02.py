85549fa8d4d5e1a83293a7987a74186cb5ba1863
Gideon Ladiyo <alwaysfun0604@gmail.com>
2023-06-17 12:02:01 +0700

    update

--- Tubes_SE06A_02.py
+++ Tubes_SE06A_02.py
@@ -1,6 +1,7 @@
 # PROYEK APLIKASI PEMESANAN TIKET KONSER AREA PURWOKERTO
 # tes
 #tes
+# tesss
 def pilih_item():
     print("===========SELAMAT DATANG==============")
     print("Pilih opsi berikut!")
