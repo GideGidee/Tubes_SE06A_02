735c513bfe953018fcdc2bb63e3af18843394756
Gideon Ladiyo <alwaysfun0604@gmail.com>
2023-06-17 11:57:36 +0700

    change

---
+++ Tubes_SE06A_02.py
@@ -0,0 +1,9 @@
+def pilih_item():
+    print("===========SELAMAT DATANG==============")
+    print("Pilih opsi berikut!")
+    print("1. Lihat tiket")
+    print("2. Pesan tiket")
+    print("3. Riwayat pemesanan")
+    print("4. Lihat pesanan")
+    print("5. Keluar")
+
