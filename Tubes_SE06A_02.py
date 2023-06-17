f14d08fc7ddad105ba87207209d21b60819dc421
Gideon Ladiyo <alwaysfun0604@gmail.com>
2023-06-17 11:49:33 +0700

    Update

---
+++ Tubes_SE06A_02.py
@@ -0,0 +1,8 @@
+def pilih_item():
+    print("========= SELAMAT DATANG DI APLIKASI PEMESANAN TIKET KONSER ==========")
+    print("Pilih pilihan anda!")
+    print("1. Lihat info tiket")
+    print("2. Pesan tiket")
+    print("3. Riwayat pemesanan")
+    print("4. Hapus pesanan")
+    print("5. Keluar")
