1067bbdc64ec781d62cb31e888d04a976956b780
MOHAMMAD DHIMAS AFRIZAL <mdhimas25@gmail.com>
2023-06-17 13:57:05 +0700

    update

--- Tubes_SE06A_02.py
+++ Tubes_SE06A_02.py
@@ -1,6 +1,6 @@
 # PROYEK APLIKASI PEMESANAN TIKET KONSER AREA PURWOKERTO
 
-def pilih_item():jjjj
+def pilih_item():
     print("===========SELAMAT DATANG==============")
     print("Pilih opsi berikut!")
     print("1. Lihat tiket")
