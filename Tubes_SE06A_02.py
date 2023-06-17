43fd9256ef4b6f2e74433e91b345e3da1561d6b6
Gideon Ladiyo <alwaysfun0604@gmail.com>
2023-06-17 12:22:57 +0700

    update

--- Tubes_SE06A_02.py
+++ Tubes_SE06A_02.py
@@ -9,4 +9,47 @@ def pilih_item():
     print("4. Lihat pesanan")
     print("5. Keluar")
 
+def tampil_tiket(tiket):
+    print("Tiket yang tersedia:")
+    for nomor, info in tiket.items():
+        print(f"Nomor Tiket: {nomor}")
+        print(f"Nama Artis: {info['artis']}")
+        print(f"Tanggal Konser: {info['tanggal']}")
+        print(f"Harga Tiket: {info['harga']}")
+        print(f"Jumlah Tiket Tersedia: {info['jumlah']}")
+        print("======================================")
 
+def pilih_tiket(tiket):
+    nomor_tiket = input("Masukkan nomor tiket yang ingin dipesan: ")
+    if nomor_tiket in tiket:
+        return nomor_tiket
+    else:
+        print("Nomor tiket tidak valid. Silakan coba lagi.")
+        return pilih_tiket(tiket)
+
+# Contoh data tiket
+tiket = {
+    "TKT001": {
+        "artis": "Artis A",
+        "tanggal": "10 Juni 2023",
+        "harga": 500000,
+        "jumlah": 10
+    },
+    "TKT002": {
+        "artis": "Artis B",
+        "tanggal": "15 Juli 2023",
+        "harga": 750000,
+        "jumlah": 5
+    },
+    "TKT003": {
+        "artis": "Artis C",
+        "tanggal": "20 Agustus 2023",
+        "harga": 1000000,
+        "jumlah": 2
+    }
+}
+
+# Contoh penggunaan
+tampil_tiket(tiket)
+nomor_tiket_terpilih = pilih_tiket(tiket)
+print("Anda telah memilih tiket dengan nomor:", nomor_tiket_terpilih)
